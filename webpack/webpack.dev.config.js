65ea424c45e122a03cce4d4a7e4b9770e927922b
jameschristou <james.christou@gmail.com>
2019-09-21 08:53:05 +1000

    Adding in HMR

--- webpack/webpack.dev.config.js
+++ webpack/webpack.dev.config.js
@@ -1,11 +1,13 @@
 const path = require('path'),
   MiniCssExtractPlugin = require('mini-css-extract-plugin'),
   UglifyJSPlugin = require('uglifyjs-webpack-plugin'),
-  OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+  OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin'),
+  HtmlWebPackPlugin = require('html-webpack-plugin'),
+  isDevelopment = process.env.NODE_ENV !== 'production';
 
 module.exports = {
   context: __dirname,
-  entry: '../src/index.js',
+  entry: ['../src/index.js'],
   output: {
     path: path.resolve(__dirname, '../dist'),
     filename: 'bundle.js'
@@ -23,15 +25,51 @@ module.exports = {
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        loader: 'babel-loader'
+        loader: 'react-hot-loader/webpack'
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env'],
+          plugins: ['@babel/plugin-transform-runtime']
+        }
       },
       {
         test: /\.s?css$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              // only enable hot in development
+              hmr: isDevelopment,
+              // if hmr does not work, this is a forceful method.
+              reloadAll: true,
+            },
+          },
+          'css-loader',
+          'sass-loader'
+        ],
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+            options: { minimize: !isDevelopment }
+          }
+        ]
       }
     ]
   },
-  plugins: [new MiniCssExtractPlugin({ filename: '../dist/style.css' })],
+  plugins: [
+    new MiniCssExtractPlugin({ filename: 'style.css' })//,
+    // new HtmlWebPackPlugin({
+    //   template: '../index.html',
+    //   filename: '../index.html'
+    // })
+  ],
   optimization: {
     minimizer: [
       new UglifyJSPlugin(),
